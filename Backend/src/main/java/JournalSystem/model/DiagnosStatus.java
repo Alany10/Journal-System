acd91783ca569b59972abc36c0f420743c1edfd3
Alinabi1 <alinabi@kth.se>
2024-11-08 13:59:58 +0100

    Changes by youal

---
+++ Backend/src/main/java/JournalSystem/model/DiagnosStatus.java
@@ -0,0 +1,7 @@
+package JournalSystem.model;
+
+public enum DiagnosStatus {
+    CONFIRMED,      // Diagnosen är fastställd
+    ONGOING,  // Diagnosen är under pågående utredning
+    OFF       // Diagnosen har uteslutits (personen har inte tillståndet)
+}
